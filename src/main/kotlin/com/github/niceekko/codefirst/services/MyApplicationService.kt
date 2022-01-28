0820ee53509c2cb4d12843ca862fce8a510412e8
GitHub Action <action@github.com>
2022-01-28 13:19:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/niceekko/codefirst/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/niceekko/codefirst/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.niceekko.codefirst.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.niceekko.codefirst.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/niceekko/codefirst/services/MyProjectService.kt
+++ src/main/kotlin/com/github/niceekko/codefirst/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.niceekko.codefirst.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.niceekko.codefirst.MyBundle
 
 class MyProjectService(project: Project) {
 
